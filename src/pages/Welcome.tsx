3bc7609683586620697ea8ae608bb35b836d439c
andersonmmendes <developer.andersonmendes@gmail.com>
2021-04-21 11:31:46 -0300

    Finalização da 2ª Aula

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,49 @@
-import React, {useState} from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
+import React from 'react';
+import { 
+  SafeAreaView, 
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet, 
+  StatusBar,
+  Dimensions,
+  View
+} from 'react-native';
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import { Button } from '../components/Button';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
-
-  function handleVisibility(){
-    setVisible(true)
+  const navigation = useNavigation();
+  
+  function handleStart(){
+    navigation.navigate('UserIdentification');
   }
 
   return (
     <>
-      <StatusBar barStyle='light-content' backgroundColor='white'/>
+      <StatusBar barStyle='dark-content' backgroundColor='white'/>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Gerencie {'\n'} suas plantas {'\n'} de forma fácil</Text>
-        {
-          visible &&
-          <Image style={styles.image} source={ wateringImg } />
-        }
-        <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. 
-          Nós cuidamos de lembrar você sempre que precisar</Text>
-        <Button title=">"/>
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Gerencie {'\n'} suas plantas de {'\n'} forma fácil</Text>
+          <Image 
+            style={styles.image} 
+            source={ wateringImg } 
+            resizeMode="contain"
+          />
+          <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. 
+            Nós cuidamos de lembrar você sempre que precisar</Text>
+          <TouchableOpacity 
+            style={styles.button} 
+            activeOpacity={0.7}
+            onPress={handleStart}  
+          >
+            <Feather name="chevron-right" sytle={styles.buttonIcon} />
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </>
   )
@@ -34,20 +54,28 @@ const styles = StyleSheet.create({
     marginTop: 10,
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
+  },
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
-    fontWeight: 'bold',
+    fontSize: 28,
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
   },
   button: {
     backgroundColor: colors.green,
@@ -57,13 +85,12 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     height: 56,
     width: 56
+  }, 
+  image: {
+    height: Dimensions.get('window').width * 0.7
   },
-   image: {
-     width: 292,
-     height: 284
-   },
-   buttonText: {
-     color: colors.white,
-     fontSize: 24,
-   }
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white
+  }
 })
